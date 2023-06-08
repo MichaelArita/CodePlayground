9756402d1b962218248a086b8f5b94ee8809faac
Michael Arita <michael.arita01@gmail.com>
2023-06-07 18:42:50 -0700

    feat: config files setup. starting to setup basic react files

---
+++ webpack.config.js
@@ -0,0 +1,60 @@
+const path = require('path');
+const HTMLWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: './client/src/index.js',
+
+  output: {
+    path: path.join(__dirname, '/client/dist'),
+    filename: 'bundle.js'
+  },
+  devServer: {
+    static: {
+      directory: path.join(__dirname, '/client/dist'),
+      publicPath: '/'
+    },
+    proxy: {
+      '/**': {
+        target: 'http://localhost:3000/',
+        secure: false,
+      },
+    },
+    compress: true,
+  },
+  plugins: [
+    new HTMLWebpackPlugin({
+      template: './client/src/index.html'
+    })
+  ],
+
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: [
+              '@babel/preset-env',
+              '@babel/preset-react'
+            ]
+          }
+        }
+      },
+      {
+        test: /\.(css|scss)$/,
+        exclude: /node_modules/,
+        use: ['style-loader', 'css-loader'],
+      },
+    //   {
+    //     test: /\.(png|jpe?g|gif)$/i,
+    //     use: [
+    //       {
+    //         loader: 'file-loader',
+    //       },
+    //     ],
+    //   }
+    ]
+  }
+};
